b714672768a13cda59e7f50037bdaab9a9d43c48
Archana Shinde <archana.m.shinde@intel.com>
2017-03-10 02:37:27 +0000

    tests: Add functional tests for shim using mock proxy.
    Add a mock proxy object to mock the proxy interaction with shim.
    The mock object serves a single shim client.
    Add make check target for the tests passing the path of the
    shim executable to "go test".
    
    Signed-off-by: Archana Shinde <archana.m.shinde@intel.com>

---
+++ tests/shim_test.go
@@ -0,0 +1,276 @@
+// Copyright (c) 2017 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net/url"
+	"os"
+	"os/exec"
+	"sync"
+	"syscall"
+	"testing"
+	"time"
+
+	"github.com/clearcontainers/shim/mock"
+	hypermock "github.com/containers/virtcontainers/hyperstart/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+type testRig struct {
+	t *testing.T
+
+	// wait for the shim to finish
+	wg sync.WaitGroup
+
+	//Mock Proxy
+	proxy *mock.Proxy
+
+	//shim forked
+	shimCommand *exec.Cmd
+
+	shimStdin  io.WriteCloser
+	shimStdout io.ReadCloser
+	shimStderr io.ReadCloser
+}
+
+func newTestRig(t *testing.T) *testRig {
+	return &testRig{
+		t: t,
+	}
+}
+
+const testContainerid = "123456789"
+const testToken = "testtoken"
+
+var shimPath string
+
+func TestMain(m *testing.M) {
+	flag.StringVar(&shimPath, "shimpath", "/usr/libexec/cc-shim",
+		"absolute path to the cc-shim executable")
+	flag.Parse()
+	fmt.Printf("Path of cc-shim executable : %s\n", shimPath)
+	os.Exit(m.Run())
+}
+
+func (rig *testRig) Start() {
+	proxySocketPath := hypermock.GetTmpPath("test-proxy.%s.sock")
+	rig.proxy = mock.NewProxy(rig.t, proxySocketPath)
+	rig.proxy.Start()
+
+	url := url.URL{
+		Scheme: "unix",
+		Path:   proxySocketPath,
+	}
+
+	rig.shimCommand = rig.getShimCommand(url.String())
+	err := rig.shimCommand.Start()
+	assert.Nil(rig.t, err)
+	rig.t.Logf("shim started\n")
+}
+
+func (rig *testRig) getShimCommand(uri string) *exec.Cmd {
+	var err error
+
+	args := []string{
+		"--container-id", testContainerid,
+		"--uri", uri,
+		"--token", testToken,
+		//"--debug",
+	}
+
+	cmd := exec.Command(shimPath, args...)
+	rig.shimStdin, err = cmd.StdinPipe()
+	assert.Nil(rig.t, err)
+	rig.shimStdout, err = cmd.StdoutPipe()
+	assert.Nil(rig.t, err)
+	rig.shimStderr, err = cmd.StderrPipe()
+	assert.Nil(rig.t, err)
+
+	return cmd
+}
+
+func (rig *testRig) Stop() {
+	cmd := rig.shimCommand
+
+	if cmd.Process != nil {
+		syscall.Kill(cmd.Process.Pid, syscall.SIGKILL)
+		rig.t.Logf("Shim process killed!")
+	}
+
+	rig.proxy.Stop()
+}
+
+func (rig *testRig) checkShimRunning() error {
+	cmd := rig.shimCommand
+	assert.NotNil(rig.t, cmd.Process)
+
+	err := syscall.Kill(cmd.Process.Pid, syscall.Signal(0))
+	if err != nil {
+		rig.t.Logf("Shim has exited early: %v\n", err)
+	}
+
+	return err
+}
+
+func TestShimConnectToProxy(t *testing.T) {
+	rig := newTestRig(t)
+	rig.Start()
+	assert.NotNil(t, rig.proxy)
+
+	defer rig.Stop()
+
+	//Check that shim is running with the command line options provided
+	time.Sleep(500 * time.Millisecond)
+	err := rig.checkShimRunning()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	//Wait for shim to connect to proxy
+	<-rig.proxy.ShimConnected
+}
+
+func TestShimStdout(t *testing.T) {
+	rig := newTestRig(t)
+	rig.Start()
+	assert.NotNil(t, rig.proxy)
+
+	defer rig.Stop()
+
+	err := rig.checkShimRunning()
+	assert.Nil(rig.t, err)
+	<-rig.proxy.ShimConnected
+
+	payload := []byte("Test stdout")
+
+	rig.proxy.SendStdoutStream(payload)
+
+	buf := make([]byte, 512)
+	n, err := rig.shimStdout.Read(buf)
+	assert.Nil(t, err)
+	assert.Equal(rig.t, payload, buf[0:len(payload)])
+
+	s := string(buf[:n])
+	t.Logf("Stdout buffer received from proxy: %s\n", s)
+}
+
+func TestShimStderr(t *testing.T) {
+	rig := newTestRig(t)
+	rig.Start()
+	assert.NotNil(t, rig.proxy)
+
+	defer rig.Stop()
+
+	err := rig.checkShimRunning()
+	assert.Nil(rig.t, err)
+	<-rig.proxy.ShimConnected
+
+	payload := []byte("Test string for stderr.")
+	rig.proxy.SendStderrStream(payload)
+
+	buf := make([]byte, 1024)
+	n, err := rig.shimStderr.Read(buf)
+	assert.Nil(t, err)
+
+	s := string(buf[:n])
+	rig.t.Logf("Stderr buffer received from proxy: %s\n", s)
+	assert.Equal(rig.t, payload, buf[0:len(payload)])
+}
+
+func TestShimSignals(t *testing.T) {
+	rig := newTestRig(t)
+	rig.Start()
+	assert.NotNil(t, rig.proxy)
+
+	defer rig.Stop()
+
+	err := rig.checkShimRunning()
+	assert.Nil(rig.t, err)
+	<-rig.proxy.ShimConnected
+
+	numSignal := 0
+	select {
+	case <-rig.proxy.Signal:
+		numSignal++
+	default:
+		t.Logf("No signals received yet")
+	}
+	assert.Equal(rig.t, numSignal, 0)
+
+	cmd := rig.shimCommand
+	assert.NotNil(rig.t, cmd.Process)
+	err = syscall.Kill(cmd.Process.Pid, syscall.SIGUSR1)
+	assert.Nil(rig.t, err)
+
+	sig := <-rig.proxy.Signal
+	assert.Equal(rig.t, syscall.Signal(sig.Signal), syscall.SIGUSR1)
+}
+
+func TestShimExitNotification(t *testing.T) {
+	rig := newTestRig(t)
+	rig.Start()
+	assert.NotNil(t, rig.proxy)
+
+	defer rig.Stop()
+
+	err := rig.checkShimRunning()
+	assert.Nil(rig.t, err)
+	<-rig.proxy.ShimConnected
+
+	payload := make([]byte, 1)
+	payload[0] = 127
+	rig.proxy.SendExitNotification(payload)
+
+	cmd := rig.shimCommand
+	assert.NotNil(rig.t, cmd.Process)
+	err = cmd.Wait()
+	assert.NotNil(rig.t, err)
+
+	<-rig.proxy.ShimDisconnected
+
+	processState := cmd.ProcessState
+	assert.NotNil(rig.t, processState)
+	ws := processState.Sys().(syscall.WaitStatus)
+	assert.NotNil(rig.t, ws)
+	exitCode := ws.ExitStatus()
+	assert.Equal(rig.t, exitCode, 127)
+}
+
+func TestShimSendingStdin(t *testing.T) {
+	rig := newTestRig(t)
+	rig.Start()
+	assert.NotNil(t, rig.proxy)
+
+	defer rig.Stop()
+
+	err := rig.checkShimRunning()
+	assert.Nil(rig.t, err)
+	<-rig.proxy.ShimConnected
+
+	stdinStream := rig.proxy.GetLastStdinStream()
+	assert.Nil(rig.t, stdinStream)
+
+	input := []byte("Test input for proxy")
+	n, err := rig.shimStdin.Write(input)
+	assert.Nil(rig.t, err)
+	assert.Equal(rig.t, n, len(input))
+
+	<-rig.proxy.StdinReceived
+	stdinStream = rig.proxy.GetLastStdinStream()
+	assert.Equal(rig.t, stdinStream, input)
+}
